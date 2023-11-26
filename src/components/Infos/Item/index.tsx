7ff56d5a05f1cf557f390d3badb3790fdbd3ec66
Felipe De Andrade <felipe.deam98@gmail.com>
2023-11-26 03:08:48 -0300

    Add Contador

--- src/components/Infos/Item/index.tsx
+++ src/components/Infos/Item/index.tsx
@@ -1,14 +1,44 @@
 import itens from 'data/infos.json';
 import styles from './Item.module.scss';
+import { useState, useEffect, FC } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 type Item = typeof itens[0];
 
-export default function Infos(props: Item) {
+const Infos: FC<Item> = (props) => {
+
+  const [contador, setContador] = useState<number>(0);
+  const [limite, setLimite] = useState<number>(props.numero);
+  const duracaoTotal = 2; // Tempo total em segundos
+  const intervalo: number = duracaoTotal / limite;
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      let tempoDecorrido = 0;
+
+      // Inicializa o intervalo quando o componente monta
+      const intervaloId = setInterval(() => {
+        if (contador < limite && tempoDecorrido <= duracaoTotal) {
+          setContador((prevContador) => prevContador + 1);
+          tempoDecorrido += intervalo;
+        } else {
+          clearInterval(intervaloId);
+        }
+      }, intervalo * 1000);
+
+      // Limpa o intervalo quando o componente desmonta
+      return () => clearInterval(intervaloId);
+    }
+  }, [contador, limite, duracaoTotal, inView]);
 
   return (
-    <div className={styles.item}>
+    <div ref={ref} className={styles.item}>
       <span>
-        {`${props.numero}${props.milhar? 'k' : '+'}`}
+        {`${contador}${props.milhar ? 'k' : '+'}`}
       </span>
       <h2>
         {props.titulo}
@@ -18,4 +48,6 @@ export default function Infos(props: Item) {
       </p>
     </div>
   );
-}
+};
+
+export default Infos;
